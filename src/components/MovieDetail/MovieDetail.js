0edfa6dccf1b8d47b2eb00ab9dafe7630ec5c2c9
catascallan <cscallan@udesa.edu.ar>
2024-09-13 09:52:14 -0300

    arreglo detail

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,29 +1,91 @@
-import React from 'react';
-import './MovieDetail.css';
+import React, { Component } from "react";
+import "./MovieDetail.css";
 
-const MovieDetail = ({ pelicula, agregarFavorito }) => {
-  if (!pelicula) {
-    return <p>No hay detalles disponibles</p>;
-  }
+class MovieDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movie: null,
+            loading: true,
+            esFavorito: false
+        };
+    }
 
-  return (
-    <section className="card-container">
-      <div className="movie-card">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`}
-          alt={pelicula.title}
-        />
-        <h4>{pelicula.title}</h4>
-        <p><strong>Calificación:</strong> {pelicula.vote_average}</p>
-        <p><strong>Fecha de lanzamiento:</strong> {pelicula.release_date}</p>
-        <p><strong>Duración:</strong> {pelicula.runtime} minutos</p>
-        <p><strong>Género:</strong> {pelicula.genres.map(g => g.name).join(', ')}</p>
-        <p>{pelicula.overview}</p>
-        <button onClick={agregarFavorito}>Agregar a Favoritos</button>
-        <a href="/">Volver al inicio</a>
-      </div>
-    </section>
-  );
-};
+    componentDidMount() {
+        this.infoPeli();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.id !== prevProps.id) {
+            this.infoPeli();
+        }
+    }
+
+    infoPeli = () => {
+        const id = this.props.id;
+        const url = `https://api.themoviedb.org/3/movie/${id}?api_key=378786c706182646715863ed0e6d66cc`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ movie: data, loading: false });
+                this.chequeoFav(data.id);
+            })
+            .catch(error => console.log(error));
+    }
+
+    chequeoFav = (id) => {
+        const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+        this.setState({ esFavorito: favoritos.includes(id) });
+    }
+
+    agregarFav = () => {
+        const { movie, esFavorito } = this.state;
+        const id = movie.id;
+        let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+        if (esFavorito) {
+            favoritos = favoritos.filter(favId => favId !== id);
+        } else {
+            favoritos.push(id);
+        }
+
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+        this.setState({ esFavorito: !esFavorito });
+    }
+
+    render() {
+        const { movie, loading, esFavorito } = this.state;
+
+        if (loading) {
+            return <p>Cargando...</p>;
+        }
+
+        if (!movie) {
+            return <p>Hubo un error al cargar los detalles de la película.</p>;
+        }
+
+        return (
+            <section className="card-container">
+                <div className="movie-card">
+                    <img
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                    />
+                    <h4>{movie.title}</h4>
+                    <p><strong>Calificación:</strong> {movie.vote_average}</p>
+                    <p><strong>Fecha de lanzamiento:</strong> {movie.release_date}</p>
+                    <p><strong>Duración:</strong> {movie.runtime} minutos</p>
+                    <p><strong>Género:</strong> {movie.genres.map(g => g.name).join(', ')}</p>
+                    <p>{movie.overview}</p>
+                    <button onClick={this.agregarFav}>
+                        {esFavorito ? "Quitar de favoritos" : "Agregar a favoritos"}
+                    </button>
+                    <a href="/">Volver al inicio</a>
+                </div>
+            </section>
+        );
+    }
+}
 
 export default MovieDetail;
--- src/pages/Detalle.js
+++ src/pages/Detalle.js
@@ -1,32 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MovieDetail from '../components/MovieDetail/MovieDetail';
 
 const Detalle = ({ match }) => {
-  const [pelicula, setPelicula] = useState(null);
-  const [cargando, setCargando] = useState(true);
-
-  const cargarPelicula = () => {
-    const idPelicula = match.params.id;
-    
-    fetch(`https://api.themoviedb.org/3/movie/${idPelicula}?api_key=3fdc54d209865d0fa99ee5f520db7d2b`)
-      .then((respuesta) => respuesta.json())
-      .then((data) => {
-        setPelicula(data);
-        setCargando(false);
-      })
-      .catch((error) => {
-        console.error('Error al obtener detalles de la película:', error);
-        setCargando(false);
-      });
-  };
-
-  if (cargando) {
-    cargarPelicula();
-    return <p>Cargando...</p>;
-  }
+  const id = match.params.id;
 
   return (
-    <MovieDetail pelicula={pelicula} />
+      <section>
+        <article>
+          <div>
+            <MovieDetail id={id} />
+          </div>
+        </article>
+      </section>
   );
 };
 
