7da211d98cd25d8710e797f8807da076fde20b6d
josefinatrezeguet <jtrezeguet@udesa.edu.ar>
2024-09-16 19:30:42 -0300

    resultado de búsquda

--- src/pages/Busqueda.js
+++ src/pages/Busqueda.js
@@ -1,44 +1,39 @@
 import { Component } from "react";
-import Peliculas from "../components/Peliculas/Peliculas"
+import Peliculas from "../components/Peliculas/Peliculas";
 
 class Busqueda extends Component {
-    constructor (props){
-        super (props) 
-
+    constructor(props) {
+        super(props);
         this.state = {
-            movies : [], 
-            isLoading: true, 
-            
-        }
+            movies: [],
+            isLoading: true,
+        };
     }
-    
-    componentDidMount () {
-        this.setState({
-            isLoading: true
-        })
-        fetch("poner api key maria {this.props.location.state.query} ") 
+
+    componentDidMount() {
+        const query = this.props.location.state.query; 
+        fetch(" https://api.themoviedb.org/3/search/movie?api_key=3fdc54d209865d0fa99ee5f520db7d2b&query=${query}") 
         .then((response) => response.json())
-        .then ((data) => {
-            this.setState ({
-                movies: data.result,
-                isLoading: false,
+            .then((data) => {
+                this.setState({
+                    movies: data.results, 
+                    isLoading: false,
+                });
             })
-        })
-        .catch((e) => console.log (e))
+            .catch((e) => console.log(e));
     }
-    
-    render(){
+
+    render() {
         return (
             <>
-            {!this.state.isLoading ?
-            (<Peliculas movies={this.state.movies}></Peliculas>)
-            : (
-                <p> Cargando...</p>
-            )
-        }
-            
+                {!this.state.isLoading ? (
+                    <Peliculas movies={this.state.movies} />
+                ) : (
+                    <p>Cargando...</p>
+                )}
             </>
-        )
+        );
     }
 }
+
 export default Busqueda;
