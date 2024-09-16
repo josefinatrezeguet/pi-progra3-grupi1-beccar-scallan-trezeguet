76b7efed288a8da7021a8778a90969f303c7b002
catascallan <cscallan@udesa.edu.ar>
2024-09-16 16:48:38 -0300

    arreglo codigo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Component } from "react";
 import Form from "../components/Forms/Form"
-
+import Peliculas from "../components/Peliculas/Peliculas";
 
 class Home extends Component{
     constructor(props){
         super(props)
     }
+
     render(){
       return(
         <>
-          <Form Form history ={this.props.history}/>
+          <Form history ={this.props.history}/>
+          <Peliculas/>
         </>
       )
     }
-
 }
+
 export default Home;
