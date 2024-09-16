405df0cf44420f315bc4beaee61a1f10f7aa7ff4
josefinatrezeguet <jtrezeguet@udesa.edu.ar>
2024-09-16 15:47:06 -0300

    Resultados de búsqueda y Loader

--- src/pages/Busqueda.js
+++ src/pages/Busqueda.js
@@ -1,7 +1,44 @@
-const Busqueda = (props) => {
-    return (
-        <>
-        </>
-    )
+import { Component } from "react";
+import Peliculas from "../components/Peliculas/Peliculas"
+
+class Busqueda extends Component {
+    constructor (props){
+        super (props) 
+
+        this.state = {
+            movies : [], 
+            isLoading: true, 
+            
+        }
+    }
+    
+    componentDidMount () {
+        this.setState({
+            isLoading: true
+        })
+        fetch("poner api key maria {this.props.location.state.query} ") 
+        .then((response) => response.json())
+        .then ((data) => {
+            this.setState ({
+                movies: data.result,
+                isLoading: false,
+            })
+        })
+        .catch((e) => console.log (e))
+    }
+    
+    render(){
+        return (
+            <>
+            {!this.state.isLoading ?
+            (<Peliculas movies={this.state.movies}></Peliculas>)
+            : (
+                <p> Cargando...</p>
+            )
+        }
+            
+            </>
+        )
+    }
 }
 export default Busqueda;
