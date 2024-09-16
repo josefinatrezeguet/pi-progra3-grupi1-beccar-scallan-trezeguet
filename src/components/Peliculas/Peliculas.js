55a1ed5867cfc40a97214a74ada668461806a117
josefinatrezeguet <jtrezeguet@udesa.edu.ar>
2024-09-16 19:23:40 -0300

    resultados de búsqueda en películas

--- src/components/Peliculas/Peliculas.js
+++ src/components/Peliculas/Peliculas.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PeliculaCard from "../PeliculaCard/PeliculaCard"; 
+import PeliculaCard from "../PeliculaCard/PeliculaCard";
 import "./Peliculas.css";
 
 const API_KEY = '3fdc54d209865d0fa99ee5f520db7d2b';
@@ -7,8 +7,8 @@ const URL_POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KE
 const URL_NOW_PLAYING = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
 
 class Peliculas extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       populares: [],
       enCartelera: [],
@@ -17,23 +17,25 @@ class Peliculas extends Component {
   }
 
   componentDidMount() {
-    fetch(URL_POPULAR)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ populares: data.results.slice(0, 5) });
-      })
-      .catch((error) => console.log(error));
+    if (!this.props.movies) {
+      fetch(URL_POPULAR)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ populares: data.results.slice(0, 5) });
+        })
+        .catch((error) => console.log(error));
 
-    fetch(URL_NOW_PLAYING)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ enCartelera: data.results.slice(0, 5) });
-      })
-      .catch((error) => console.log(error));
+      fetch(URL_NOW_PLAYING)
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ enCartelera: data.results.slice(0, 5) });
+        })
+        .catch((error) => console.log(error));
 
       const favoritos = localStorage.getItem("favoritos") 
-      ? JSON.parse(localStorage.getItem("favoritos")) : [];
-        this.setState({ favoritos });
+        ? JSON.parse(localStorage.getItem("favoritos")) : [];
+      this.setState({ favoritos });
+    }
   }
 
   agregarFav = (id) => {
@@ -53,46 +55,69 @@ class Peliculas extends Component {
 
   render() {
     const { populares, enCartelera } = this.state;
+    const { movies } = this.props; 
 
     return (
       <div className="peliculas">
-        <section>
-          <h2>PELÍCULAS POPULARES</h2>
-          <div className="contenedor-peliculas">
-            {populares.length > 0 ? (
-              populares.map((peli) => (
-                <PeliculaCard
-                  key={peli.id}
-                  pelicula={peli}
-                  esFavorito={this.esFavorito}
-                  agregarFav={this.agregarFav}
-                />
-              ))
-            ) : (
-              <p>Cargando...</p>
-            )}
-          </div>
-          <a href="/more/category/popular">Ver Todas - Películas más populares</a>
-        </section>
+        {movies ? (
+          <section>
+            <h2>RESULTADOS DE BUSQUEDA</h2>
+            <div className="contenedor-peliculas">
+              {movies.length > 0 ? (
+                movies.map((peli) => (
+                  <PeliculaCard
+                    key={peli.id}
+                    pelicula={peli}
+                    esFavorito={this.esFavorito}
+                    agregarFav={this.agregarFav}
+                  />
+                ))
+              ) : (
+                <p>No se encontraron resultados.</p>
+              )}
+            </div>
+          </section>
+        ) : (
+          <>
+            <section>
+              <h2>PELÍCULAS POPULARES</h2>
+              <div className="contenedor-peliculas">
+                {populares.length > 0 ? (
+                  populares.map((peli) => (
+                    <PeliculaCard
+                      key={peli.id}
+                      pelicula={peli}
+                      esFavorito={this.esFavorito}
+                      agregarFav={this.agregarFav}
+                    />
+                  ))
+                ) : (
+                  <p>Cargando...</p>
+                )}
+              </div>
+              <a href="/more/category/popular">Ver Todas - Películas más populares</a>
+            </section>
 
-        <section>
-          <h2>PELÍCULAS EN CARTELERA</h2>
-          <div className="contenedor-peliculas">
-            {enCartelera.length > 0 ? (
-              enCartelera.map((peli) => (
-                <PeliculaCard
-                  key={peli.id}
-                  pelicula={peli}
-                  esFavorito={this.esFavorito}
-                  agregarFav={this.agregarFav}
-                />
-              ))
-            ) : (
-              <p>Cargando...</p>
-            )}
-          </div>
-          <a href="/more/category/now_playing">Ver Todas - Películas en cartelera</a>
-        </section>
+            <section>
+              <h2>PELÍCULAS EN CARTELERA</h2>
+              <div className="contenedor-peliculas">
+                {enCartelera.length > 0 ? (
+                  enCartelera.map((peli) => (
+                    <PeliculaCard
+                      key={peli.id}
+                      pelicula={peli}
+                      esFavorito={this.esFavorito}
+                      agregarFav={this.agregarFav}
+                    />
+                  ))
+                ) : (
+                  <p>Cargando...</p>
+                )}
+              </div>
+              <a href="/more/category/now_playing">Ver Todas - Películas en cartelera</a>
+            </section>
+          </>
+        )}
       </div>
     );
   }
