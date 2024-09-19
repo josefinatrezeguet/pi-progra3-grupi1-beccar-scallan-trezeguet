280b32edc4d457fac190ed1d42ee3116d076439e
catascallan <cscallan@udesa.edu.ar>
2024-09-19 19:35:26 -0300

    corrección navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
             <ul className="main-nav">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/favorites">Favoritos</Link></li>
-                <li><Link to="/see-more/category/popular">Ver todas las películas más populares</Link></li>
-                <li><Link to="/see-more/category/now_playing">Ver todas las películas en cartelera</Link></li>
+                <li><Link to="/more/category/popular">Ver todas las películas más populares</Link></li>
+                <li><Link to="/more/category/now_playing">Ver todas las películas en cartelera</Link></li>
             </ul>
         </nav>
     );
