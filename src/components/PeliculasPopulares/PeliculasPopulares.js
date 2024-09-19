20dfdbe01d99ed2b553522e2c639e25e9f129e48
rociobeccarv <rbeccarvarela@udesa.edu.ar>
2024-09-18 21:54:55 -0300

    arreglar peliculas populares

--- src/components/PeliculasPopulares/PeliculasPopulares.js
+++ src/components/PeliculasPopulares/PeliculasPopulares.js
@@ -1,67 +1,97 @@
-import PeliculasPopularesCard from "../PeliculaCard/PeliculaCard";
-import React, {Component} from "react";
-
+import React, { Component } from "react";
+import PeliculaCard from "../PeliculaCard/PeliculaCard";
+import "./PeliculasPopulares.css";
 
 const API_KEY = '3fdc54d209865d0fa99ee5f520db7d2b';
-const peliculasPopulares = `⁠ https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY} ⁠`;
-
+const URLS = {
+  popular: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+  now_playing:`⁠https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`,
+};
 
 class PeliculasPopulares extends Component {
-    constructor() {
-        super();
-        this.state = {
-            populares: [],
-            verMas: false,
-            textoBoton: "ver mas"
-            
-        };
-    }
-    handleVerMas(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      filteredMovies: [],
+      filterValue: "",
+      favoritos: [],
+    };
+  }
+
+  componentDidMount() {
+    const { category } = this.props.match.params;
+    const url = URLS[category];
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
-            verMas: !this.state.verMas 
-        })
+          movies: data.results,
+          filteredMovies: data.results,
+        });
+      })
+      .catch((error) => console.log(error));
+
+    const favoritos = localStorage.getItem("favoritos")
+      ? JSON.parse(localStorage.getItem("favoritos"))
+      : [];
+    this.setState({ favoritos });
+  }
+
+  esFavorito = (id) => {
+    return this.state.favoritos.includes(id);
+  };
+
+  agregarFav = (id) => {
+    let { favoritos } = this.state;
+    if (favoritos.includes(id)) {
+      favoritos = favoritos.filter((favoritoId) => favoritoId !== id);
+    } else {
+      favoritos.push(id);
     }
-    componentDidMount() {
-        fetch(peliculasPopulares)
-            .then(response => response.json())
-            .then(data => {this.setState(
-                { populares: data.results } 
-            )})
-            .catch((error) => console.log(error));
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    this.setState({ favoritos });
+  };
 
-        
-    }
-    render() {
+  handleFilterChange = (event) => {
+    const filterValue = event.target.value.toLowerCase();
+    this.setState((prevState) => ({
+      filterValue,
+      filteredMovies: prevState.movies.filter((movie) =>
+        movie.title.toLowerCase().includes(filterValue)
+      ),
+    }));
+  };
 
-        const {populares, verMas} = this.state;
-        const masPelis = verMas ? populares: populares.slice(0,5); 
-        return(
-            <>
-                <section className="card-container">
-                    <h2>Películas Populares</h2>
-                <div> 
-                    {masPelis.length > 0 ? (
-                        masPelis.map((pelicula) => (
-                            <PeliculasPopularesCard
-                                key={pelicula.id}
-                                pelicula={pelicula}
-                            />
-                        ))
-                    ) : (
-                        <p></p> 
-                    )}
-                </div>
-                <button onClick={() => this.handleVerMas()}> {verMas ? 'ver menos' : 'ver más'} </button>
-                
-            </section>
+  render() {
+    const { filteredMovies, filterValue } = this.state;
+    const { category } = this.props.match.params;
 
-           
-            </>
+    const pageTitle = category === 'popular'
+      ? 'Películas populares' : category === 'now_playing'
+      ? 'Películas en cartelera' : 'Películas'; 
 
-        )
-    }
+    return (
+      <div className="peliculas-page">
+        <h2>{pageTitle}</h2>
+        <div className="contenedor-peliculas">
+          {filteredMovies.length > 0 ? (
+            filteredMovies.map((peli) => (
+              <PeliculaCard
+                key={peli.id}
+                pelicula={peli}
+                esFavorito={this.esFavorito}
+                agregarFav={this.agregarFav}
+              />
+            ))
+          ) : (
+            <p>No se encontraron películas.</p>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-
 export default PeliculasPopulares;
-
